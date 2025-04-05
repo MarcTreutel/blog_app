21cdfd62d688f6004ff0f785760f58a114c35130
Marc Treutel <marc.treutel@outlook.de>
2025-04-05 21:48:14 +0200

    Add environment variable APP_SECRET_KEY and add Procfile

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, CreateRegistrationForm, CreateLoginForm, CreateCommentForm
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("APP_SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -248,5 +249,5 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
     
